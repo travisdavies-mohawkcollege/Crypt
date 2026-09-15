cb56a46144c05b25ef6d6b688f7bfc6691f2e3f4
travisdavies-mohawkcollege <travis.davies@mohawkcollege.ca>
2026-09-15 17:01:59 -0400

    Add basic dungeon generation

---
+++ Crypt/Assets/Scripts/Dungeon/Generation/RoomData.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+//This is essentially each room's memory.
+//This can store variables we want to base room
+//decorations, and the RoomView reads what cells this
+//room is connected to in order too open walls/place doors.
+
+public class RoomData
+{
+    public Vector3Int Cell;
+    public HashSet<Vector3Int> ConnectedCells = new();
+    public Vector3Int EntryDirection { get; set; }
+    public bool IsStairway {  get ; set; }
+
+    public RoomData(Vector3Int cell)
+    {
+        this.Cell = cell;
+        EntryDirection = Vector3Int.zero;
+    }
+}
